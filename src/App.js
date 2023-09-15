bb1b0de0195fc8cbcbecb6441c593327dbae2c7d
Anuj <anujtiwari2822@gmail.com>
2023-09-15 20:25:55 +0530

    Signin/sign up form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Body from './components/Body';
 
 function App() {
   return (
-    <div className="text-3xl font-bold">
-    Netflix-gpt
+    <div>
+    <Body/>
     </div>
   );
 }
